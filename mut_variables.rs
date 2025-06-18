1f767464504b9287cb72d2ccf6faaf5f8ab996be
j0zko <jozefgunar952@gmail.com>
2025-06-18 19:07:52 +0200

    Variables:Practising declaring a mutable variable with "mut"

---
+++ mut_variables.rs
@@ -0,0 +1,13 @@
+/*
+   Mutable variables
+   value of mutable can be changed
+    Declaration of a mutable variable with "mut"
+
+*/
+
+fn main() {
+    let mut answer = "YES";
+    println!("First answer {}", answer);
+    answer = "NO";
+    println!("Second answer {}", answer);
+}
