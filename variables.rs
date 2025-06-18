07376e5a72c89613761c89e51d0ef95b587d9dd1
j0zko <jozefgunar952@gmail.com>
2025-06-18 18:43:17 +0200

    Variables : Practising declaring immutable variables with "let"

---
+++ variables.rs
@@ -0,0 +1,13 @@
+/*
+    Inmutable variables in rust
+    are by default in rust ,
+    they value cannot be changed once a value is bond to it
+
+*/
+
+fn main() {
+    let employment = 500;
+    println!("all of this people is empleyed {}", employment);
+    let percentage: f64 = 34.00;
+    println!("the percentages of unimplemented! {}", percentage);
+}
