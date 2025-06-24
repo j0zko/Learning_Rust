6626666a9ba72f441acdf694d2721a325e63de0c
j0zko <jozefgunar952@gmail.com>
2025-06-24 09:32:09 +0200

    Functions: feat :Implementing a return functing to return value 50

--- functions.rs
+++ functions.rs
@@ -8,8 +8,15 @@
 fn main() {
     //calling the function hello
     hello();
+    println!("returning { }", returning_function());
 }
 
 fn hello() {
     println!("hello");
+}
+
+fn returning_function() -> i32 {
+    let a = 20;
+    let b = 30;
+    a + b
 }
