98fc59f1cc5f249a6fa973454d3226ee5757e3c8
j0zko <jozefgunar952@gmail.com>
2025-06-24 08:16:13 +0200

    Functions : feat: functions in rust and calling my own function inside the main
    - functions without params
    -calling my own inside the main

---
+++ functions.rs
@@ -0,0 +1,15 @@
+/*
+*   FUNCTIONS
+*   functions are reusable blocks of code and can be used many times
+*
+*
+* */
+
+fn main() {
+    //calling the function hello
+    hello();
+}
+
+fn hello() {
+    println!("hello");
+}
